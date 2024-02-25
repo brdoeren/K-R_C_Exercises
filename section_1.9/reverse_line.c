d685c27a014b2b6bfac89dc1b0db64d6c0bbadb1
Brent Doeren <155897301+brdoeren@users.noreply.github.com>
2024-02-25 09:26:25 -0500

    added get_line function

--- section_1.9/reverse_line.c
+++ section_1.9/reverse_line.c
@@ -7,16 +7,23 @@ time */
 
 #define MAX_LINE_LENGTH 1000
 
+/* Might be better to make reverse into
+a two argument function, one with input
+and one with modified output. But exercise
+asks for a single arg function */
 int reverse(char line[]);
+int get_line_with_arbitrary_length(char s[], int limit);
 
 int main()
 {
     int current_length, i;
     char line[MAX_LINE_LENGTH];
 
-    while((current_length = reverse(line)) > 0)
+    /* Using previously written get_line function */
+    while((current_length = get_line_with_arbitrary_length(line, MAX_LINE_LENGTH)) > 0)
     {
-        printf("%s\n", line);
+        reverse(line);
+        printf("%s", line);
     }
 
     return 0;
@@ -28,4 +35,43 @@ it returns the length of the line */
 int reverse(char s[])
 {
     return 0;
+}
+
+/* Read as much of the line into s as is within the limit;
+return the length of line even exceeding the limit */
+int get_line_with_arbitrary_length(char s[], int limit)
+{
+    int c, i, end_of_line;
+
+    /* for loop continues past limit to EOF or newline
+    i continues to track arbitrary length */
+    for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+    {
+        if (i < limit - 1)
+        {
+            s[i] = c;
+        }
+    }
+
+    /* Best fit needs to include space for '\n' and '\0' */
+    if (i > limit - 2)
+    {
+        end_of_line = limit - 2;
+    }
+    else
+    {
+        end_of_line = i;
+    }
+
+    if (c == '\n')
+    {
+        s[end_of_line] = c;
+        ++end_of_line;
+        /* Increment i so that \n is
+        counted as one character */
+        ++i;
+    }
+
+    s[end_of_line] = '\0';
+    return i;
 }
