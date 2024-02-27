1b258741b66bf0b78c7f521eee24b886509dd930
Brent Doeren <155897301+brdoeren@users.noreply.github.com>
2024-02-26 19:36:22 -0500

    start detab exercise

---
+++ section_1.10/detab.c
@@ -0,0 +1,14 @@
+/* Write a program _detab_ that replaces tabs
+in the input with the proper number of blanks
+to space to the next tab spot. Assume a fixed
+set of tab stops, say every 'n' columns. Should
+'n' be a variable or a symbolic parameter? */
+
+#include <stdio.h>
+
+#define TAB_STOP_SPACES 4
+
+int main()
+{
+    return 0;
+}
