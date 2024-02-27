153074deca02cb679ae5031b408ffc070f797af0
Brent Doeren <155897301+brdoeren@users.noreply.github.com>
2024-02-27 10:25:04 -0500

    notes concerning the goal of the convert function

--- section_1.10/detab.c
+++ section_1.10/detab.c
@@ -15,17 +15,47 @@ void convert_tabs_to_spaces(char source[], char result[], int tab_stop_spaces);
 int main()
 {
     char line[MAX_LINE_LENGTH];
+    char converted_line[MAX_LINE_LENGTH];
 
     while(get_line_with_arbitrary_length(line, MAX_LINE_LENGTH) > 0)
     {
         printf("%s", line);
+        convert_tabs_to_spaces(line, converted_line, TAB_STOP_SPACES);
+        printf("%s", converted_line);
     }
 
     return 0;
 }
 
+/* Takes string s and converts the tabs in s into spaces
+and stores the result in r, tab_stop_spaces indicates the
+number of blanks per fixed tab stop. Gives best fit result
+when the length of r exceeds MAX_LINE_LENGTH */
 void convert_tabs_to_spaces(char s[], char r[], int tab_stop_spaces)
 {
+    int length, i, j;
+    for(length = 0; s[length] != '\0'; ++length);
+
+    for(i = 0; i < length; ++i)
+    {
+        /*
+        Find tabs in original line
+        Replace tabs with spaces...
+        But make spaces line up at tab stop
+        We know a tab stop is every 'tab_stop_spaces'
+        So taking the current index of the result array
+        and dividing it with 'tbs' should give the index
+        that the spaces should align with in order to
+        match the tab stop
+        And modulo of current result index should give
+        the offset of the current index from the
+        previous tab stop
+        */
+        if (s[i] == '\t')
+        {
+
+        }
+    }
 
 }
 
