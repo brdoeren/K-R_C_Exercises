eee1bc094a5a890ea31584e9ae4e721376484a59
Brent Doeren <155897301+brdoeren@users.noreply.github.com>
2024-02-27 19:02:49 -0500

    detab now works as expected, sans edge cases

--- section_1.10/detab.c
+++ section_1.10/detab.c
@@ -19,7 +19,6 @@ int main()
 
     while(get_line_with_arbitrary_length(line, MAX_LINE_LENGTH) > 0)
     {
-        printf("%s", line);
         convert_tabs_to_spaces(line, converted_line, TAB_STOP_SPACES);
         printf("%s", converted_line);
     }
@@ -38,25 +37,11 @@ void convert_tabs_to_spaces(char s[], char r[], int tab_stop_spaces)
 
     for(i = 0, j = 0; i < length + 1; ++i)
     {
-        /*
-        Find tabs in original line
-        Replace tabs with spaces...
-        But make spaces line up at tab stop
-        We know a tab stop is every 'tab_stop_spaces'
-        So taking the current index of the result array
-        and dividing it with 'tbs' should give the index
-        that the spaces should align with in order to
-        match the tab stop
-        And modulo of current result index should give
-        the offset of the current index from the
-        previous tab stop
-        */
-
         if (s[i] == '\t')
         {
             /* Tabs are converted into spaces
             that lead up to the next tab stop */
-            int number_blanks = tab_stop_spaces - (i % tab_stop_spaces);
+            int number_blanks = tab_stop_spaces - (j % tab_stop_spaces);
             while (number_blanks > 0)
             {
                 r[j] = ' ';
