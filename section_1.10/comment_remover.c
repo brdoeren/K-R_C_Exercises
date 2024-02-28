7fa6d2bdb8006c9c2f327c1519d16f2d326c438a
Brent Doeren <155897301+brdoeren@users.noreply.github.com>
2024-02-28 10:37:59 -0500

    add skeletons of other chapter exercises

---
+++ section_1.10/comment_remover.c
@@ -0,0 +1,15 @@
+/* Write a program to remove
+all comments from a C program.
+Don't forget to handle quoted
+strings and character constants
+properly. C comments do not nest. */
+
+/* TODO: this will be a simplified
+program without using file i/o.
+Come back and rewrite this when
+going over the chapters that deal
+with file i/o */
+int main()
+{
+    return 0;
+}
---
+++ section_1.10/syntax_checker.c
@@ -0,0 +1,18 @@
+/* Write a program to check a C
+program for rudimentary syntax
+errors like  unbalanced parentheses,
+brackets and braces. Don't forget
+about quotes, both single and double,
+escape sequences, and comments.
+(This program is hard if you do it in
+ full generality.) */
+
+/* TODO: this will be a simplified
+program without using file i/o.
+Come back and rewrite this when
+going over the chapters that deal
+with file i/o */
+int main()
+{
+    return 0;
+}
